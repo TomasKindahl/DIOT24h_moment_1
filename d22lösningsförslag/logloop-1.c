fa0dab88c3cd573b377b5dc0b608cc07d45b037f
tomas.kindahl@bahnhof.se <tomas.kindahl@bahnhof.se>
2024-11-05 08:09:32 +0100

    fixme xor

---
+++ d22lösningsförslag/logloop-1.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+    char line[256];
+    int b1, b2;
+    do {
+        printf("ange kommando: ");
+        fgets(line, 256, stdin);
+        if(0 == strncmp(line, "quit", 4))
+            break;
+        else if(0 == strncmp(line, "xor", 3)) {
+            sscanf(line, "xor %d %d", &b1, &b2);
+            printf("  %B ^ %B == %B\n", b1, b2, b1^b2);
+        }
+    } while(1);
+    return 0;
+}
